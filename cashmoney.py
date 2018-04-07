15b013e7842dbf7484becd754d09f824e2ffd9db
jaythaceo <jaythaceo@gmail.com>
2018-04-07 12:34:24 -0400

    [chore] Added Binary search to cashmoney

--- cashmoney.py
+++ cashmoney.py
@@ -1,4 +1,4 @@
-"""
+# Binary Search 
 def binarySearch(alist, item):
 	first = 0
 	last = len(alist) -1
@@ -18,5 +18,3 @@ def binarySearch(alist, item):
 testList = [0,1,2,3,45,3,5,234]
 print(binarySearch(testList, 3))
 print(binarySearch(testList, 25))
-"""
-print(23 /2 )
